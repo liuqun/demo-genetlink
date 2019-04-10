4d2f915c8b390b0ed9fdc637fb14f5d98ffe5482
Liu Qun <517067180@qq.com>
2019-04-10 02:04:23 -0700

    patch: 修正data参数长度值
    对字符串长度额外加1字节的逻辑，主程序已经做过一次，所以demo_send_cmd()中不应该再加1字节,
    否则发送32位整数data时也调用这个demo_send_cmd()函数, 会产生内核警告信息如下:
        netlink: 'demo_genetlink_': attribute type 2 has an invalid length.
    
    fixes: https://github.com/luckyapple1028/demo-genetlink/issues/2

--- demo_genetlink_user.c
+++ demo_genetlink_user.c
@@ -92,7 +92,7 @@ static int demo_send_cmd(int sd, __u16 nlmsg_type, __u32 nlmsg_pid,
 	msg.g.version = DEMO_GENL_VERSION;
 	na = (struct nlattr *) GENLMSG_DATA(&msg);
 	na->nla_type = nla_type;
-	na->nla_len = nla_len + 1 + NLA_HDRLEN;
+	na->nla_len = nla_len + NLA_HDRLEN;
 	memcpy(NLA_DATA(na), nla_data, nla_len);
 	msg.n.nlmsg_len += NLMSG_ALIGN(na->nla_len);
 
